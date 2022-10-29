160cb201e22b1ce7e2ea7123105dd1dd75bb125a
Caleb Brazeau <51797165+CalebBrazeau@users.noreply.github.com>
2022-10-29 10:46:02 -0400

    Detect Mouse Input

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] bool boolIsPlaceable;
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
-        
+        if (boolIsPlaceable)
+        {
+            Debug.Log(transform.name);
+        }
     }
 }
